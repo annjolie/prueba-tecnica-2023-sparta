1e21eba23cd9026d8194d084c3ff635eea2d4522
annjolie <annjolie.lourido@gmail.com>
2023-04-17 12:36:54 +0200

    comments and fixed test

--- src/logic/reducers.ts
+++ src/logic/reducers.ts
@@ -1,6 +1,9 @@
 import { authReducer } from './auth';
 import { tasksReducer } from './tasks';
 
+// combina todos los reducers en uno solo para ser usado en el store de redux y poder acceder a todos
+// los estados desde cualquier componente de la aplicacion sin necesidad de pasarlos por props
+
 export default {
   authReducer,
   tasksReducer
